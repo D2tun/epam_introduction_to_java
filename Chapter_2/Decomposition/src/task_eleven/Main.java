28f3eb81b72a9d4fb655550a413b1e94faa02ae3
D2tun <71644776+D2tun@users.noreply.github.com>
2021-07-06 14:48:26 +0300

    Update Main.java

--- Chapter_2/Decomposition/src/task_eleven/Main.java
+++ Chapter_2/Decomposition/src/task_eleven/Main.java
@@ -1,6 +1,6 @@
 package task_eleven;
 
-/* Написать метод(ы), определяющий, в каком из данных двух чисел больше цифр. */
+/* РќР°РїРёСЃР°С‚СЊ РјРµС‚РѕРґ(С‹), РѕРїСЂРµРґРµР»СЏСЋС‰РёР№, РІ РєР°РєРѕРј РёР· РґР°РЅРЅС‹С… РґРІСѓС… С‡РёСЃРµР» Р±РѕР»СЊС€Рµ С†РёС„СЂ. */
 
 import global.Input_value_int;
 
@@ -9,20 +9,20 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int n1 = 0;
-		System.out.print("Введите первое число ");
+		System.out.print("Р’РІРµРґРёС‚Рµ РїРµСЂРІРѕРµ С‡РёСЃР»Рѕ ");
 		String num1 = String.valueOf(Input_value_int.Input_value(n1));
 		int n2 = 0;
-		System.out.print("Введите второе число ");
+		System.out.print("Р’РІРµРґРёС‚Рµ РІС‚РѕСЂРѕРµ С‡РёСЃР»Рѕ ");
 		String num2 = String.valueOf(Input_value_int.Input_value(n2));
 
 		if (num1.length() > num2.length()) {
-			System.out.println("В первом числе больше цифр.");
+			System.out.println("Р’ РїРµСЂРІРѕРј С‡РёСЃР»Рµ Р±РѕР»СЊС€Рµ С†РёС„СЂ.");
 		}
 		if (num1.length() < num2.length()) {
-			System.out.println("Во втором числе больше цифр.");
+			System.out.println("Р’Рѕ РІС‚РѕСЂРѕРј С‡РёСЃР»Рµ Р±РѕР»СЊС€Рµ С†РёС„СЂ.");
 		}
 		if (num1.length() == num2.length()) {
-			System.out.println("В обоих числах одинаковое количество цифр.");
+			System.out.println("Р’ РѕР±РѕРёС… С‡РёСЃР»Р°С… РѕРґРёРЅР°РєРѕРІРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ С†РёС„СЂ.");
 		}
 	}
 
