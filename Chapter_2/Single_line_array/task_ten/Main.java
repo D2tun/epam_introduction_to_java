f1d56940608fdf17c841794db761cf97e5d3ce68
D2tun <71644776+D2tun@users.noreply.github.com>
2021-07-06 14:40:27 +0300

    Add files via upload

---
+++ Chapter_2/Single_line_array/task_ten/Main.java
@@ -0,0 +1,38 @@
+package task_ten;
+
+/* Дан целочисленный массив с количеством элементов n. Сжать массив, выбросив из него каждый
+второй элемент (освободившиеся элементы заполнить нулями). Примечание. Дополнительный
+массив не использовать. */
+
+import global.Input_value_int;
+
+public class Main {
+
+	public static void main (String[] args) {
+		
+		int c = 1;
+		int n = 0;
+		System.out.print("Введите размер массива ");
+		n = StrictMath.abs(Input_value_int.Input_value(n));
+		
+		System.out.println();
+		
+		int[] arr = new int [n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = (int) StrictMath.round(StrictMath.random()*101);
+			System.out.print(arr[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for (int i  = 0; i < n; i++, c++) {
+			if (c > 2) {
+				c = 1;
+			}
+			if (c == 2) {
+				arr[i] = 0;
+			}
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
