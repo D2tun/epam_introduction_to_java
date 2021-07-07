6b35a695a83867170313f59725f0af55414755e1
D2tun <71644776+D2tun@users.noreply.github.com>
2021-07-07 10:46:46 +0300

    Update Main.java

--- Chapter_4/simple_classes_and_objects/src/task_ten/Main.java
+++ Chapter_4/simple_classes_and_objects/src/task_ten/Main.java
@@ -1,13 +1,13 @@
 package task_ten;
 
-/* Создать класс Airline, спецификация которого приведена ниже. Определить конструкторы, set- и get-
-методы и метод toString(). Создать второй класс, агрегирующий массив типа Airline, с подходящими конструкторами
-и методами. Задать критерии выбора данных и вывести эти данные на консоль.
- Airline: пункт назначения, номер рейса, тип самолёта, время вылета, дни недели.
- Найти и вывести:
- a) список рейсов для заданного пункта назначения;
- b) список рейсов для заданного дня недели;
- c) список рейсов для заданного дня недели, время вылета для которых больше заданного.*/
+/* РЎРѕР·РґР°С‚СЊ РєР»Р°СЃСЃ Airline, СЃРїРµС†РёС„РёРєР°С†РёСЏ РєРѕС‚РѕСЂРѕРіРѕ РїСЂРёРІРµРґРµРЅР° РЅРёР¶Рµ. РћРїСЂРµРґРµР»РёС‚СЊ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂС‹, set- Рё get-
+РјРµС‚РѕРґС‹ Рё РјРµС‚РѕРґ toString(). РЎРѕР·РґР°С‚СЊ РІС‚РѕСЂРѕР№ РєР»Р°СЃСЃ, Р°РіСЂРµРіРёСЂСѓСЋС‰РёР№ РјР°СЃСЃРёРІ С‚РёРїР° Airline, СЃ РїРѕРґС…РѕРґСЏС‰РёРјРё РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°РјРё
+Рё РјРµС‚РѕРґР°РјРё. Р—Р°РґР°С‚СЊ РєСЂРёС‚РµСЂРёРё РІС‹Р±РѕСЂР° РґР°РЅРЅС‹С… Рё РІС‹РІРµСЃС‚Рё СЌС‚Рё РґР°РЅРЅС‹Рµ РЅР° РєРѕРЅСЃРѕР»СЊ.
+ Airline: РїСѓРЅРєС‚ РЅР°Р·РЅР°С‡РµРЅРёСЏ, РЅРѕРјРµСЂ СЂРµР№СЃР°, С‚РёРї СЃР°РјРѕР»С‘С‚Р°, РІСЂРµРјСЏ РІС‹Р»РµС‚Р°, РґРЅРё РЅРµРґРµР»Рё.
+ РќР°Р№С‚Рё Рё РІС‹РІРµСЃС‚Рё:
+ a) СЃРїРёСЃРѕРє СЂРµР№СЃРѕРІ РґР»СЏ Р·Р°РґР°РЅРЅРѕРіРѕ РїСѓРЅРєС‚Р° РЅР°Р·РЅР°С‡РµРЅРёСЏ;
+ b) СЃРїРёСЃРѕРє СЂРµР№СЃРѕРІ РґР»СЏ Р·Р°РґР°РЅРЅРѕРіРѕ РґРЅСЏ РЅРµРґРµР»Рё;
+ c) СЃРїРёСЃРѕРє СЂРµР№СЃРѕРІ РґР»СЏ Р·Р°РґР°РЅРЅРѕРіРѕ РґРЅСЏ РЅРµРґРµР»Рё, РІСЂРµРјСЏ РІС‹Р»РµС‚Р° РґР»СЏ РєРѕС‚РѕСЂС‹С… Р±РѕР»СЊС€Рµ Р·Р°РґР°РЅРЅРѕРіРѕ.*/
 
 import java.util.Scanner;
 
@@ -19,56 +19,56 @@ public class Main {
 		
 		Airline airline1 = new Airline();
 		airline1.set_airline_number(134);
-		airline1.set_desination("Москва");
-		airline1.set_airplane_type("ТУ-134");
-		airline1.set_day_of_week("сб.");
+		airline1.set_desination("РњРѕСЃРєРІР°");
+		airline1.set_airplane_type("РўРЈ-134");
+		airline1.set_day_of_week("СЃР±.");
 		airline1.set_start_time("13:00");
 		airlines.add_airline(airline1);
 		
 		Airline airline2 = new Airline();
 		airline2.set_airline_number(166);
-		airline2.set_desination("Хургада");
+		airline2.set_desination("РҐСѓСЂРіР°РґР°");
 		airline2.set_airplane_type("Boeing 737-800");
-		airline2.set_day_of_week("пт.");
+		airline2.set_day_of_week("РїС‚.");
 		airline2.set_start_time("01:00");
 		airlines.add_airline(airline2);
 		
 		Airline airline3 = new Airline();
 		airline3.set_airline_number(170);
-		airline3.set_desination("Рим");
+		airline3.set_desination("Р РёРј");
 		airline3.set_airplane_type("Embarer E-195");
-		airline3.set_day_of_week("пт.");
+		airline3.set_day_of_week("РїС‚.");
 		airline3.set_start_time("09:45");
 		airlines.add_airline(airline3);
 		
 		Airline airline4 = new Airline();
 		airline4.set_airline_number(140);
-		airline4.set_desination("Багдад");
+		airline4.set_desination("Р‘Р°РіРґР°Рґ");
 		airline4.set_airplane_type("Boeing 737-800");
-		airline4.set_day_of_week("пн.");
+		airline4.set_day_of_week("РїРЅ.");
 		airline4.set_start_time("16:00");
 		airlines.add_airline(airline4);
 		
 		Airline airline5 = new Airline();
 		airline5.set_airline_number(180);
-		airline5.set_desination("Ларнака");
+		airline5.set_desination("Р›Р°СЂРЅР°РєР°");
 		airline5.set_airplane_type("Embarer E-175");
-		airline5.set_day_of_week("вс.");
+		airline5.set_day_of_week("РІСЃ.");
 		airline5.set_start_time("08:55");
 		airlines.add_airline(airline5);
 		
 		Scanner sc = new Scanner(System.in);
-		/*System.out.println("Введите пункт назначения.");
+		/*System.out.println("Р’РІРµРґРёС‚Рµ РїСѓРЅРєС‚ РЅР°Р·РЅР°С‡РµРЅРёСЏ.");
 		String a = sc.nextLine();
 		airlines.airlines_to(a);
 		
-		System.out.println("Введите день недели (например: пн.).");
+		System.out.println("Р’РІРµРґРёС‚Рµ РґРµРЅСЊ РЅРµРґРµР»Рё (РЅР°РїСЂРёРјРµСЂ: РїРЅ.).");
 		String b = sc.nextLine();
 		airlines.airlines_at_the_day_of_the_week(b);*/
 		
-		System.out.println("Введите день недели (например: пн.).");
+		System.out.println(Р’РІРµРґРёС‚Рµ РґРµРЅСЊ РЅРµРґРµР»Рё (РЅР°РїСЂРёРјРµСЂ: РїРЅ.).");
 		String c = sc.nextLine();
-		System.out.println("Введите время.");
+		System.out.println("Р’РІРµРґРёС‚Рµ РІСЂРµРјСЏ.");
 		String d = sc.nextLine();
 		airlines.airlines_at_the_day_of_the_week_after_time(c, d);
 		
