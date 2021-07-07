8c0708536f2fdc67629693462ddbe807f59a3b08
D2tun <71644776+D2tun@users.noreply.github.com>
2021-07-07 07:27:02 +0300

    Update Input_value_int.java

--- Chapter_2/Multiple_line_array/src/global/Input_value_int.java
+++ Chapter_2/Multiple_line_array/src/global/Input_value_int.java
@@ -1,6 +1,5 @@
 package global;
 
-
 import java.util.Scanner;
 
 public class Input_value_int {
@@ -9,7 +8,7 @@ public class Input_value_int {
 
 		while (!sc.hasNextInt()) {
 			sc.next();
-			System.out.println("Неверный формат данных");
+			System.out.println("РќРµРІРµСЂРЅС‹Р№ С„РѕСЂРјР°С‚ РґР°РЅРЅС‹С…");
 		}
 		value = sc.nextInt();
 		return value;
