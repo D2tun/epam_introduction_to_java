1a9e6fa20d0f7b7fefde60a0da1eb814db3f04f8
D2tun <71644776+D2tun@users.noreply.github.com>
2021-09-03 14:31:09 +0300

    Update Main.java

--- Chapter_3/RegularExeptions/src/by/minsk/epam/jio/taskTwo/Main.java
+++ Chapter_3/RegularExeptions/src/by/minsk/epam/jio/taskTwo/Main.java
@@ -1,5 +1,3 @@
-package by.minsk.epam.jio.taskTwo;
-
 /* 
  * Дана строка, содержащая следующий текст (xml-документ). Напишите анализатор,
  * позволяющий последовательно возвращать содержимое узлов xml-документа и его
@@ -7,24 +5,26 @@ package by.minsk.epam.jio.taskTwo;
  * Пользоваться готовыми парсерами XML для решения данной задачи нельзя.
  */
 
+package by.minsk.epam.jio.taskTwo;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		String xmlDoc = "<notes>\n"
-						 + " <note id = \"1\">\n" 
-						 + "  <to>Вася</to>\n" 
-						 + "  <from>Света</from>\n"
-						 + "  <heading>Напоминание</heading>\n"  
-						 + "  <body>Позвони мне завтра!</body>\n" 
-						 + " </note>\n" 
-						 + " <note id = \"2\">\n" 
-						 + "  <to>Петя</to>\n" 
-						 + "  <from>Маша</from>\n" 
-						 + "  <heading>Важное напоминание!</heading>\n" 
-						 + "  <body/>\n" 
-						 + " </note>\n" 
-						 + "</notes>";
+				+ " <note id = \"1\">\n" 
+				+ "  <to>Вася</to>\n" 
+				+ "  <from>Света</from>\n"
+				+ "  <heading>Напоминание</heading>\n"  
+				+ "  <body>Позвони мне завтра!</body>\n" 
+				+ " </note>\n" 
+				+ " <note id = \"2\">\n" 
+				+ "  <to>Петя</to>\n" 
+				+ "  <from>Маша</from>\n" 
+				+ "  <heading>Важное напоминание!</heading>\n" 
+				+ "  <body/>\n" 
+				+ " </note>\n" 
+				+ "</notes>";
 		
 		System.out.println("Открывающие теги: " 
 						  + XMLAnalyzer.readOpeningTags(xmlDoc));
