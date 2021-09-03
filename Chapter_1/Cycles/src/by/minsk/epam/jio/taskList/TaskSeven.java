e67b10d8a0f72b97faa12eb9cccdbaa06d45245e
D2tun <71644776+D2tun@users.noreply.github.com>
2021-09-03 08:00:29 +0300

    Update TaskSeven.java

--- Chapter_1/Cycles/src/by/minsk/epam/jio/taskList/TaskSeven.java
+++ Chapter_1/Cycles/src/by/minsk/epam/jio/taskList/TaskSeven.java
@@ -1,9 +1,9 @@
-package by.minsk.epam.jio.taskList;
-
 /*
  * Для каждого натурального числа в промежутке от m до n вывести все делители,
  * кроме единицы и самого числа. m и n вводятся с клавиотуры.
  */
+
+package by.minsk.epam.jio.taskList;
 
 import by.minsk.epam.jio.input.InputValueInt;
 
@@ -25,7 +25,7 @@ public class TaskSeven {
 		while ((number >= m) & (number <= n)) {
 			System.out.print("Делители " + number + ": ");
 			for (div = 2; div < (number); div++) {
-				if (StrictMath.floorMod(number, div) == 0 & div != n & div != m) {
+				if ((StrictMath.floorMod(number, div) == 0) & (div != n) & (div != m)) {
 					System.out.print(div + ", ");
 				}
 			}
